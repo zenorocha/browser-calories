7ca695bf9f497158708e1e44dee2f1a7be32e449
Zeno Rocha <hi@zenorocha.com>
2016-05-13 12:44:55 -0700

    Migrates DOM initialization #9

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,6 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 import App from './app';
 
-ReactDOM.render(<App/>, document.querySelector('#wrapper'));
+new App({}, document.querySelector('#wrapper'));
