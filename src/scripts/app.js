e68ecedecb9331b0cfcd9065325f560a255b92bc
Andre Alves Garzia <contato@andregarzia.com>
2016-05-16 13:22:06 -0300

    Working version. Fixed problem with chrome.storage.sync not being implemented on Firefox.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,13 +6,11 @@ import defaultBudget from '../data/budget';
 
 class App extends JSXComponent {
   attached() {
-    console.log("Attached...");
     this.getURL();
     this.getBudget();
   }
 
   getURL() {
-    console.log("GetURL...");
     chrome.tabs.query({
       active: true,
       currentWindow: true
@@ -24,8 +22,8 @@ class App extends JSXComponent {
   }
 
   getBudget() {
-    console.log("GetBudget...");
-    chrome.storage.sync.get(defaultBudget, (data) => {
+    chrome.storage.local.get(defaultBudget, (data) => {
+      console.log(data);
       this.setState({
         budget: data
       });
@@ -33,27 +31,20 @@ class App extends JSXComponent {
   }
 
   fetchURL() {
-    console.log("fetchURL...");
     let api = 'https://calories.browserdiet.com/?url=' + encodeURIComponent(this.url);
 
     fetch(api)
       .then((response) => {
-        console.log("return from fetch");
-        console.log(response);
         return response.json();
       })
       .then((response) => {
-        console.log("after fetch");
-        console.log(response);
         
         // Response doesn't contain status code in here.
         if (!response.hasOwnProperty("html") ) {
-          console.log("error!!!!");
           this.setState({
             error: response
           });         
         } else {
-          console.log("success");
           this.setState({
             success: response
           });
@@ -62,22 +53,13 @@ class App extends JSXComponent {
   }
 
   render() {
-    /*
-    State is not being set or accessed. All calls such as `this.success` return undefined.
-    */
-    console.log("Render...");
-    console.log(this.success);
-    console.log(this.budget);
     if (this.success && this.budget) {
-      console.log("success on render");
       return <Result url={this.url} success={this.success} budget={this.budget} />;
     }
     else if (this.error) {
-      console.log("fail on render");
       return <Failure url={this.url} error={this.error} />;
     }
     else {
-      console.log("loader on render: " + this.url); 
       return <Loader url={this.url} />;
     }
   }
