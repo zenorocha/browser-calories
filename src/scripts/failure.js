6a6e17c8dafb5aef307c214c876106befdf7bad6
Zeno Rocha <hi@zenorocha.com>
2016-05-30 07:12:37 -0700

    Encodes error message and url when reporting an issue

--- src/scripts/failure.js
+++ src/scripts/failure.js
@@ -14,7 +14,7 @@ class Failure extends JSXComponent {
         <img class="status-rays" src="../images/rays.png" alt="Rays" />
         <div class="status-msg">
           <p>{title}</p>
-          <p>Try again on another tab or fill an <a href={"https://github.com/zenorocha/browser-calories/issues/new?title=Error+" + this.config.error.code + "+for+" + this.config.url + "&body=%3E+" + this.config.error.message} target="_blank">issue on GitHub</a></p>
+          <p>Try again on another tab or fill an <a href={"https://github.com/zenorocha/browser-calories/issues/new?title=Error+" + this.config.error.code + "+for+" + encodeURIComponent(this.config.url) + "&body=%3E+" + encodeURIComponent(this.config.error.message)} target="_blank">issue on GitHub</a></p>
         </div>
       </div>
     )
